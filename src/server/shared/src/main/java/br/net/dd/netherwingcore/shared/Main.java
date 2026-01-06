9070f28417cf107198136d57dfcabb4ef76d51a8
Idair F. Guido <idair_guido@hotmail.com>
2026-01-06 09:11:31 -0300

    Add Protobuf JSON serializer/deserializer utilities
      - Introduced Serializer and Deserializer classes for converting between Protobuf messages and JSON using Gson.
      - Updated pom.xml to include dependencies for protobuf, gRPC, and Gson.
      - Moved Main.java to a shared package to reflect new structure.

--- src/server/shared/src/main/java/br/net/dd/netherwingcore/shared/Main.java
+++ src/server/shared/src/main/java/br/net/dd/netherwingcore/shared/Main.java
@@ -1,4 +1,4 @@
-package br.net.dd.netherwingcore;
+package br.net.dd.netherwingcore.shared;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
