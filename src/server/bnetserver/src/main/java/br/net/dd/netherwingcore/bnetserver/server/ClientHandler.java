a980fb36537fa0ca3a61d010cc4284b95f547d71
Idair F. Guido <idair_guido@hotmail.com>
2026-02-10 14:18:35 -0300

    Parse RPC header and log SSL session info
      - Start TLS handshake, parse a RpcTypesProto.Header from the client socket and log header fields (serviceId, methodId, token, clientId).
      - Replace DataInputStream with a BufferedReader for input, keep DataOutputStream for output, and add detailed SSLSession logging (session ID, cipher suite, protocol, peer host/port).
      - Also add required imports for proto, logging and Arrays.

--- src/server/bnetserver/src/main/java/br/net/dd/netherwingcore/bnetserver/server/ClientHandler.java
+++ src/server/bnetserver/src/main/java/br/net/dd/netherwingcore/bnetserver/server/ClientHandler.java
@@ -1,10 +1,15 @@
 package br.net.dd.netherwingcore.bnetserver.server;
 
+import br.net.dd.netherwingcore.common.logging.InformationMessage;
+import br.net.dd.netherwingcore.common.logging.LogFile;
+import br.net.dd.netherwingcore.proto.client.RpcTypesProto;
 import com.google.rpc.context.AttributeContext;
 
+import javax.net.ssl.SSLParameters;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
 import java.io.*;
+import java.util.Arrays;
 
 import static br.net.dd.netherwingcore.common.logging.Log.log;
 
@@ -35,12 +40,30 @@ public class ClientHandler implements Runnable {
     public void run() {
         // Use try-with-resources to ensure streams are closed properly
         try (
-                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+                BufferedReader in = new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
                 DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream())
         ) {
             log("Client connected: " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
 
-            log("All Bytes: "+in.readAllBytes().length);
+            clientSocket.startHandshake();
+
+            RpcTypesProto.Header header = RpcTypesProto.Header.parseFrom(clientSocket.getInputStream());
+
+            log("Service ID: " + header.getServiceId());
+            log("Method ID: " + header.getMethodId());
+            log("Token: " + header.getToken());
+
+            log("Handshake request received: " + header.getClientId());
+
+            SSLSession sslSession = clientSocket.getSession();
+
+            log("Client SSL Session Info: " + sslSession.toString());
+
+            log("Client SSL Session ID: " + Arrays.toString(sslSession.getId()));
+            log("Client SSL Cipher Suite: " + sslSession.getCipherSuite());
+            log("Client SSL Protocol: " + sslSession.getProtocol());
+            log("Client SSL Peer Host: " + sslSession.getPeerHost());
+            log("Client SSL Port Number: " + sslSession.getPeerPort());
 
         } catch (IOException e) {
             log("Error handling client: " + e.getMessage());
